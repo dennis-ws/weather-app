60feab60559fe9c9fc6fc888acbeab41c0513938
Dennis <dennissung6105@gmail.com>
2025-01-21 00:04:36 +1000

    Added helper container functions for App.js

---
+++ src/weather/weatherContainer.js
@@ -0,0 +1,12 @@
+import { styled } from '@mui/system'
+import { Container } from '@mui/material'
+
+export const TodayContainer = styled(Container)`
+flex: 0 0 66%; /* Takes up 75% of the width */
+background-color: lightgreen
+
+`;
+
+export const WeekContainer = styled(Container)`
+flex: 0 0 34%; /* Takes up 25% of the width */
+`;
